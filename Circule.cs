18b45e5a401d180e2196c7cff0f45a6cf68b92b2
MHY Express <mcyalda@yahoo.com>
2018-05-04 11:43:03 -0400

    first commit

---
+++ Circule.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10
+{
+    class Circule
+    {
+        private double radius;
+        
+        // constractor
+        public Circule(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public Circule()
+        {
+            Radius = 0;
+        }
+
+        // propertes 
+        public double Radius
+
+        {
+            set
+            {
+                radius = value;
+            }
+            get
+            {
+                return radius;
+            }
+        }
+
+       // creat  two methods
+        public double CalculateCircumference()
+        {
+            double pi = Math.PI;
+            double circumference = 2 * pi * radius;
+            return circumference;
+        }
+
+        public double CalculateArea()
+        {
+            double pi = Math.PI;
+            double area = pi * (radius * radius);
+            return area;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\twlecome to the Circle Tester !!");
+            int count = 0;
+            bool repeat = true;
+            while (repeat)
+            {
+                count++;
+                Console.Write("enter Radius ?");
+                double radius = double.Parse(Console.ReadLine());
+                           
+                Circule objects = new Circule(radius);
+
+                Console.WriteLine($"Circumference:{objects.CalculateCircumference()}\n");
+
+                Console.WriteLine($"Area:{objects.CalculateArea()}\n");
+
+                repeat = Confirmation("Continue? (y/n)");
+            }
+            Console.WriteLine($"You Created {count} Circule Object");
+        }
+
+        //method for y or n
+        public static bool Confirmation(string message)
+        {
+            Console.WriteLine(message);
+            string input1 = Console.ReadLine().ToLower();
+            while ((!Regex.IsMatch(input1, @"^[n|y]$")))
+            {
+                Console.WriteLine("invalid entery please enter the 'y' or 'n')");
+                input1 = Console.ReadLine();
+            }
+            return input1 == "y";
+        }
+
+        //method for is the string number between 0-9
+        //public static double RadiusConfirmation(string message)
+        //{
+        //    Console.WriteLine(message);
+        //    string input = Console.ReadLine();
+        //    while ((!Regex.IsMatch(input, @"^[0-9]{1,30}$")))
+        //    {
+        //        Console.WriteLine("invalid entery please enter the 'y' or 'n')");
+        //        input = Console.ReadLine();
+        //    }
+        //    return input;
+        //}
+    }
+}
